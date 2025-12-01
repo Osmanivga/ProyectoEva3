6b34d8cf2622f81fcf6276609084fe959fb665c1
Osman Gallardo <osman.gallardo02@inacapmail.cl>
2025-12-01 13:28:48 -0300

    creacion de admin Producto e Importacion del Format_html

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from .models import Categoria, Producto, Insumo, Pedido, ImagenReferencia
+from django.utils.html import format_html
 
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ("nombre",)
     search_fields= ("nombre",)
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "mostrar_imagen", "categoria", "precio_base")
+    search_fields = ("nombre", "descripcion")
+    list_filter = ("categoria",)
+
+    
+    def mostrar_imagen(self, obj):
+        if obj.imagen1:
+            
+            return format_html('<img src="{}" width="50" height="50" style="border-radius:5px;" />', obj.imagen1.url)
+        return "Sin imagen"
+    mostrar_imagen.short_description = "Portada"
+
+
