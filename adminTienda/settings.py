826c821a1ef7343ece82f3e2339235949c501789
Osman Gallardo <osman.gallardo02@inacapmail.cl>
2025-12-15 00:18:48 -0300

    actualizacion de sttings al mysql

--- adminTienda/settings.py
+++ adminTienda/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'DJANGO_API',
         'USER': 'root',
-        'PASSWORD': 'pass'
+        'PASSWORD': ''
 
     }
 }
